c10991c59a4a0ee88d46c135f71a6df1d16e2b5a
TYoungboysitinh <133793185+TYoungboysitinh@users.noreply.github.com>
2023-10-01 16:02:50 +0700

    Update ListTask.js

--- src/Components/ListTask.js
+++ src/Components/ListTask.js
@@ -1,15 +1,20 @@
 import React from 'react'
 
-export default function ListTask() {
+export default function ListTask({renderTask, rollNo}) {
+    let elementLevel = <span className='label label-danger'>High</span>
+    if(renderTask.level === 2){
+        elementLevel = <span className='label label-info'>Medium</span>
+    }else if(renderTask.level === 3){
+        elementLevel = <span className='label label-default'>Small</span>
+    }
     return (
             <tr>
-                <td className="text-center">3</td>
+                <td className="text-center">{rollNo}</td>
                 <td>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
-                    ea c
+                    {renderTask.taskName}
                 </td>
                 <td className="text-center">
-                    <span className="label label-info">Medium</span>
+                    <span>{elementLevel}</span>
                 </td>
                 <td>
                     <button type="button" className="btn btn-warning">
