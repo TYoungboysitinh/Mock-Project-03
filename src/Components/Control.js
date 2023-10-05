df2a966a542a0903079540f1f16fd1b3a8d7b9d2
TYoungboysitinh <133793185+TYoungboysitinh@users.noreply.github.com>
2023-10-06 00:40:28 +0700

    Update Control.js

--- src/Components/Control.js
+++ src/Components/Control.js
@@ -1,20 +1,20 @@
-  import React, { useState } from 'react'
-  import Search from './Control/Search'
-  import Sort from './Control/Sort'
-  import Add from './Control/Add'
+import React, { useState } from 'react'
+import Search from './Control/Search'
+import Sort from './Control/Sort'
+import Add from './Control/Add'
 
-  export default function Control({onAddTask, onSearch}) {
+export default function Control({onAddTask, onSearch, onSort}) {
 
-    // Hàm xử lý sự kiện Add
-    const handleAddTask = (toggle,actionName, task)=>{
-      onAddTask (toggle,actionName, task);
-    }
-
-    return (
-      <div className='row'>
-          <Search onSearch={onSearch} />
-          <Sort />
-          <Add  onAddTask={handleAddTask}/>
-      </div>
-    )
+  // Hàm xử lý sự kiện Add
+  const handleAddTask = (toggle,actionName, task)=>{
+    onAddTask (toggle,actionName, task);
   }
+
+  return (
+    <div className='row'>
+        <Search onSearch={onSearch} />
+        <Sort onSort={onSort} />
+        <Add  onAddTask={handleAddTask}/>
+    </div>
+  )
+}
