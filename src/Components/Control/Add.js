d84d9feca631acebd8af796ad16eab703b81fa2e
TYoungboysitinh <133793185+TYoungboysitinh@users.noreply.github.com>
2023-10-05 20:50:32 +0700

    Update Add.js

--- src/Components/Control/Add.js
+++ src/Components/Control/Add.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Add() {
+export default function Add({onAddTask}) {
+    // Hàm xử lý sự kiện khi người dùng click vào nút Add Task 
+    const handleAdd = () => {
+        onAddTask(true, "Save", null);
+    }
+
     return (
         <div className="col-xs-5 col-sm-5 col-md-5 col-lg-5">
-            <button type="button" className="btn btn-info btn-block">
+            <button type="button" className="btn btn-info btn-block" onClick={handleAdd}>
                 Add Task
             </button>
         </div>
