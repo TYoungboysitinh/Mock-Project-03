0a649e6b458ca87f4ad559b93869aac5ce513136
TYoungboysitinh <133793185+TYoungboysitinh@users.noreply.github.com>
2023-10-05 20:45:43 +0700

    Update Control.js

--- src/Components/Control.js
+++ src/Components/Control.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import Search from './Control/Search'
-import Sort from './Control/Sort'
-import Add from './Control/Add'
+  import React, { useState } from 'react'
+  import Search from './Control/Search'
+  import Sort from './Control/Sort'
+  import Add from './Control/Add'
 
-export default function Control() {
-  return (
-    <div className='row'>
-        <Search />
-        <Sort />
-        <Add />
-    </div>
-  )
-}
+  export default function Control({onAddTask, onSearch}) {
+
+    // Hàm xử lý sự kiện Add
+    const handleAddTask = (toggle,actionName, task)=>{
+      onAddTask (toggle,actionName, task);
+    }
+
+    return (
+      <div className='row'>
+          <Search onSearch={onSearch} />
+          <Sort />
+          <Add  onAddTask={handleAddTask}/>
+      </div>
+    )
+  }
